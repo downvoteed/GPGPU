fd6d0f5fb801e446ad1e060f499992341da3dca4
Vincent Thirouin <vincent.thirouin@epita.fr>
2023-06-03 19:28:13 +0200

    chore(cou): cleanup with clang-format llvm

--- include/frame-helper.hh
+++ include/frame-helper.hh
@@ -2,162 +2,149 @@
 
 #include <opencv2/opencv.hpp>
 
-namespace frame_helper
-{
-    using frames = std::vector<cv::Mat>;
-    using frames_ref = std::vector<cv::Mat *>;
-    using frames_vector = std::vector<frames>;
+namespace frame_helper {
+using frames = std::vector<cv::Mat>;
+using frames_ref = std::vector<cv::Mat *>;
+using frames_vector = std::vector<frames>;
 
-    /**
-     * Read a frame from a file
-     * @param path The path to the file
-     * @param gray Whether to read the frame in grayscale
-     * @return The OpenCV frame
-     */
-    cv::Mat readFrame(const std::string &path, bool gray = false)
-    {
-        int flag = cv::IMREAD_COLOR;
-        if (gray)
-        {
-            flag = cv::IMREAD_GRAYSCALE;
-        }
+/**
+ * Read a frame from a file
+ * @param path The path to the file
+ * @param gray Whether to read the frame in grayscale
+ * @return The OpenCV frame
+ */
+cv::Mat readFrame(const std::string &path, bool gray = false) {
+  int flag = cv::IMREAD_COLOR;
+  if (gray) {
+    flag = cv::IMREAD_GRAYSCALE;
+  }
 
-        cv::Mat frame = cv::imread(path, flag);
-        if (frame.empty())
-        {
-            std::cout << "Could not read the image: " << path << std::endl;
-            exit(1);
-        }
+  cv::Mat frame = cv::imread(path, flag);
+  if (frame.empty()) {
+    std::cout << "Could not read the image: " << path << std::endl;
+    exit(1);
+  }
 
-        return frame;
+  return frame;
+}
+
+/**
+ * Show a frame in a window
+ * @param name The name of the window
+ * @param frame The OpenCV frame
+ */
+void showFrame(const std::string &name, const cv::Mat &frame) {
+  cv::namedWindow(name, cv::WINDOW_AUTOSIZE);
+  cv::imshow(name, frame);
+  cv::waitKey(0);
+
+  cv::destroyWindow(name);
+}
+
+/**
+ * Build a segmented frame from a vector of boolean values
+ * (foreground/background)
+ * @param segments The vector of boolean values (1 or 0)
+ * @param width The width of the frame
+ * @param height The height of the frame
+ * @return The segmented frame
+ */
+cv::Mat buildSegmentedFrame(const std::vector<uint8_t> &segments, int width,
+                            int height) {
+  cv::Mat frame = cv::Mat::zeros(height, width, CV_8UC1);
+  for (unsigned long i = 0; i < segments.size(); i++) {
+    int c = i % width;
+    int r = i / width;
+    frame.at<uint8_t>(r, c) = segments[i] * 255;
+  }
+
+  return frame;
+}
+
+/**
+ * Read a video file and return its frames
+ * @param path The path to the video file
+ * @param width The width of the frames (optional)
+ * @param height The height of the frames (optional)
+ * @return The frames
+ */
+frames_vector readFrames(const std::string &path,
+                         const std::optional<int> &width = std::nullopt,
+                         const std::optional<int> &height = std::nullopt) {
+  cv::VideoCapture video(path);
+  if (!video.isOpened()) {
+    std::cout << "Error opening video stream or file" << std::endl;
+    exit(1);
+  }
+
+  frames colored_frames = {};
+  frames gray_frames = {};
+
+  // Read the video frame by frame
+  while (true) {
+    cv::Mat frame;
+    video >> frame;
+
+    // Break the loop at the end of the video
+    if (frame.empty()) {
+      break;
     }
 
-    /**
-     * Show a frame in a window
-     * @param name The name of the window
-     * @param frame The OpenCV frame
-     */
-    void showFrame(const std::string &name, const cv::Mat &frame)
-    {
-        cv::namedWindow(name, cv::WINDOW_AUTOSIZE);
-        cv::imshow(name, frame);
-        cv::waitKey(0);
-
-        cv::destroyWindow(name);
+    // Resize the frame if needed
+    if (width.has_value() || height.has_value()) {
+      int w = width.value_or(frame.cols);
+      int h = height.value_or(frame.rows);
+      cv::resize(frame, frame, cv::Size(w, h));
     }
 
-    /**
-     * Build a segmented frame from a vector of boolean values (foreground/background)
-     * @param segments The vector of boolean values (1 or 0)
-     * @param width The width of the frame
-     * @param height The height of the frame
-     * @return The segmented frame
-     */
-    cv::Mat buildSegmentedFrame(const std::vector<uint8_t> &segments, int width, int height)
-    {
-        cv::Mat frame = cv::Mat::zeros(height, width, CV_8UC1);
-        for (int i = 0; i < segments.size(); i++)
-        {
-            int c = i % width;
-            int r = i / width;
-            frame.at<uint8_t>(r, c) = segments[i] * 255;
-        }
+    // Add the frame to the frames vector
+    colored_frames.push_back(frame);
 
-        return frame;
+    // Convert the frame to grayscale and add it to the frames vector
+    cv::Mat gray_frame;
+    cv::cvtColor(frame, gray_frame, cv::COLOR_BGR2GRAY);
+    gray_frames.push_back(gray_frame);
+  }
+
+  // Release the video capture object
+  video.release();
+
+  // Return the frames
+  return {colored_frames, gray_frames};
+}
+
+/**
+ * Save frames to a video file
+ * @param path The path to the video file
+ * @param frames The frames to save
+ */
+void saveFrames(const std::string &path, frames_ref &frames) {
+  cv::Size frameSize(frames[0]->cols, frames[0]->rows);
+  cv::VideoWriter video(path, cv::VideoWriter::fourcc('m', 'p', '4', 'v'), 24,
+                        frameSize);
+  if (!video.isOpened()) {
+    std::cout << "Error opening video stream or file" << std::endl;
+    exit(1);
+  }
+
+  // Write the frames to the video
+  for (unsigned long i = 0; i < frames.size(); i++) {
+    auto frame = frames[i];
+    cv::Mat bgrFrame;
+
+    // Check if the frame is grayscale (single channel)
+    if (frames[i]->channels() == 1) {
+      // Convert grayscale to BGR
+      cv::cvtColor(*frame, bgrFrame, cv::COLOR_GRAY2BGR);
+    } else {
+      bgrFrame = *(frames[i]);
     }
 
-    /**
-     * Read a video file and return its frames
-     * @param path The path to the video file
-     * @param width The width of the frames (optional)
-     * @param height The height of the frames (optional)
-     * @return The frames
-     */
-    frames_vector readFrames(const std::string &path, const std::optional<int> &width = std::nullopt, const std::optional<int> &height = std::nullopt)
-    {
-        cv::VideoCapture video(path);
-        if (!video.isOpened())
-        {
-            std::cout << "Error opening video stream or file" << std::endl;
-            exit(1);
-        }
+    video.write(bgrFrame);
+  }
 
-        frames colored_frames = {};
-        frames gray_frames = {};
-
-        // Read the video frame by frame
-        while (true)
-        {
-            cv::Mat frame;
-            video >> frame;
-
-            // Break the loop at the end of the video
-            if (frame.empty())
-            {
-                break;
-            }
-
-            // Resize the frame if needed
-            if (width.has_value() || height.has_value())
-            {
-                int w = width.value_or(frame.cols);
-                int h = height.value_or(frame.rows);
-                cv::resize(frame, frame, cv::Size(w, h));
-            }
-
-            // Add the frame to the frames vector
-            colored_frames.push_back(frame);
-
-            // Convert the frame to grayscale and add it to the frames vector
-            cv::Mat gray_frame;
-            cv::cvtColor(frame, gray_frame, cv::COLOR_BGR2GRAY);
-            gray_frames.push_back(gray_frame);
-        }
-
-        // Release the video capture object
-        video.release();
-
-        // Return the frames
-        return {colored_frames, gray_frames};
-    }
-
-    /**
-     * Save frames to a video file
-     * @param path The path to the video file
-     * @param frames The frames to save
-     */
-    void saveFrames(const std::string &path, frames_ref &frames)
-    {
-        cv::Size frameSize(frames[0]->cols, frames[0]->rows);
-        cv::VideoWriter video(path, cv::VideoWriter::fourcc('m', 'p', '4', 'v'), 24, frameSize);
-        if (!video.isOpened())
-        {
-            std::cout << "Error opening video stream or file" << std::endl;
-            exit(1);
-        }
-
-        // Write the frames to the video
-        for (int i = 0; i < frames.size(); i++)
-        {
-            auto frame = frames[i];
-            cv::Mat bgrFrame;
-
-            // Check if the frame is grayscale (single channel)
-            if (frames[i]->channels() == 1)
-            {
-                // Convert grayscale to BGR
-                cv::cvtColor(*frame, bgrFrame, cv::COLOR_GRAY2BGR);
-            }
-            else
-            {
-                bgrFrame = *(frames[i]);
-            }
-
-            video.write(bgrFrame);
-        }
-
-        // Release the video writer object
-        video.release();
-    }
+  // Release the video writer object
+  video.release();
+}
 
 } // namespace frame_helper
--- include/segmentation-helper.hh
+++ include/segmentation-helper.hh
@@ -1,59 +1,57 @@
 #pragma once
 
-#include <vector>
 #include <color-helper.hh>
 #include <texture-helper.hh>
+#include <vector>
 
-namespace segmentation_helper
-{
-    using layer_vector = std::vector<uint8_t>;
+namespace segmentation_helper {
+using layer_vector = std::vector<uint8_t>;
 
-    /**
-     * Segment a frame into foreground and background
-     * @param color_similarities The color similarities between the current frame and the background frame
-     * @param texture_similarities The texture similarities between the current frame and the background frame
-     * @return The a vector of boolean values (1 or 0) representing the foreground or the background
-     */
-    layer_vector segment(color_helper::similarity_vectors color_similarities, texture_helper::similarity_vector texture_similarities)
-    {
-        layer_vector segments;
+/**
+ * Segment a frame into foreground and background
+ * @param color_similarities The color similarities between the current frame
+ * and the background frame
+ * @param texture_similarities The texture similarities between the current
+ * frame and the background frame
+ * @return The a vector of boolean values (1 or 0) representing the foreground
+ * or the background
+ */
+layer_vector segment(color_helper::similarity_vectors color_similarities,
+                     texture_helper::similarity_vector texture_similarities) {
+  layer_vector segments;
 
-        // Define the factors for the color and texture similarities
-        std::vector<double> factors = {0.1, 0.3, 0.6};
+  // Define the factors for the color and texture similarities
+  std::vector<double> factors = {0.1, 0.3, 0.6};
 
-        // Calculate the weighted sum of the color and texture similarities
-        for (int i = 0; i < color_similarities[0].size(); i++)
-        {
-            double r = color_similarities[0][i];
-            double g = color_similarities[1][i];
-            double t = texture_similarities[i];
+  // Calculate the weighted sum of the color and texture similarities
+  for (unsigned long i = 0; i < color_similarities[0].size(); i++) {
+    double r = color_similarities[0][i];
+    double g = color_similarities[1][i];
+    double t = texture_similarities[i];
 
-            // Sort the similarities in ascending order
-            std::vector<double> similarities = {r, g, t};
-            std::sort(similarities.begin(), similarities.end());
+    // Sort the similarities in ascending order
+    std::vector<double> similarities = {r, g, t};
+    std::sort(similarities.begin(), similarities.end());
 
-            // Multiply the similarities with the factors
-            for (int j = 0; j < similarities.size(); j++)
-            {
-                similarities[j] *= factors[j];
-            }
-
-            // Calculate the weighted sum of the similarities and threshold it
-            double similarity = similarities[0] + similarities[1] + similarities[2];
-
-            // If the similarity is greater than 0.67, it is the foreground
-            if (similarity >= 0.67)
-            {
-                segments.push_back(1);
-            }
-            // Otherwise, it is the background
-            else
-            {
-                segments.push_back(0);
-            }
-        }
-
-        return segments;
+    // Multiply the similarities with the factors
+    for (unsigned long j = 0; j < similarities.size(); j++) {
+      similarities[j] *= factors[j];
     }
+
+    // Calculate the weighted sum of the similarities and threshold it
+    double similarity = similarities[0] + similarities[1] + similarities[2];
+
+    // If the similarity is greater than 0.67, it is the foreground
+    if (similarity >= 0.67) {
+      segments.push_back(0);
+    }
+    // Otherwise, it is the background
+    else {
+      segments.push_back(1);
+    }
+  }
+
+  return segments;
+}
 
 } // namespace segmentation_helper
